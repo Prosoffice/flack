8d151e256c48bc353246e8a86fe70c4b1c927433
prosoffice <ogbonnayaprosper1@gmail.com>
2020-09-11 18:22:48 +0100

    CSRF token fix

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-from flask import Flask, render_template, redirect, session, Session, request
+from flask import Flask, render_template, redirect, session, request
 from flask_socketio import SocketIO, emit, join_room, leave_room
 from flask_bootstrap import Bootstrap
 from flask_wtf.csrf import CSRFProtect
